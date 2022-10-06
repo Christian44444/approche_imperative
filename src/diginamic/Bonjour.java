5810e6225b054786812ba460590638d27537aaa1
Christian44444 <c.michaud@laposte.net>
2022-10-06 15:36:23 +0200

    Init

---
+++ src/diginamic/Bonjour.java
@@ -0,0 +1,14 @@
+package diginamic;
+
+public class Bonjour {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bonjour tout le monde ");
+		System.out.println("complétion ctrl + espace");
+		int a = 053; // notation financière 100_000_000
+		System.out.println(a); // Notation octale 0<ton nombre>, attention pour l'hexa 0x<ton nombre>, en binaire 0b<ton nombre> 
+		
+	}
+
+}
