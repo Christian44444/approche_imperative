000b0e31639942457f177ae12cbbacbcfe0def08
Christian44444 <c.michaud@laposte.net>
2022-10-06 16:42:10 +0200

    Création classe DeclarationApp

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,25 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte by1 = 1;
+		short sh1 = 2000;
+		int i1 = 30000;
+		long l1 = 400000;
+		float f1 = -500.245F;
+		double d1 = 200.100;
+		char c1 = 'A';
+		boolean bo1 = false;
+		String st1 = "Toto";
+		System.out.println("sh1");
+		String randomString = "Voici le résultat d’un calcul : 1+5=6";
+		System.out.println(randomString);
+		randomString = "Voici le résultat d’un calcul :\n1+5=6";
+		System.out.println(randomString);
+		
+	}
+
+}
